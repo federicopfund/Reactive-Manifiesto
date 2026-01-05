947cf208ed6306aaef6e463ab93e32ef79bf4316
Federico Christian Pfund <74926730+federicopfund@users.noreply.github.com>
2026-01-05 23:10:11 +0000

    HomeController.scala: Agregar endpoints API (GET /api/contacts, GET /api/contacts/stats)

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -7,6 +7,7 @@ import play.api.data._
 import play.api.data.Forms._
 import play.api.i18n.I18nSupport
 import services.ReactiveContactAdapter
+import repositories.ContactRepository
 import core.{Contact, ContactSubmitted, ContactError}
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -16,7 +17,8 @@ case class ContactFormData(name: String, email: String, message: String)
 @Singleton
 class HomeController @Inject()(
   val controllerComponents: ControllerComponents,
-  adapter: ReactiveContactAdapter
+  adapter: ReactiveContactAdapter,
+  contactRepository: ContactRepository
 )(implicit ec: ExecutionContext) extends BaseController with I18nSupport {
 
   // Form definition
@@ -31,6 +33,26 @@ class HomeController @Inject()(
 
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index(contactForm))
+  }
+
+  def publicaciones() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.publicaciones())
+  }
+
+  def publicacion(slug: String) = Action { implicit request: Request[AnyContent] =>
+    slug match {
+      case "akka-actors" => Ok(views.html.articulos.akkaActors())
+      case "patrones-resiliencia" => Ok(views.html.articulos.patronesResiliencia())
+      case "akka-streams" => Ok(views.html.articulos.akkaStreams())
+      case "play-async" => Ok(views.html.articulos.playAsync())
+      case "message-passing" => Ok(views.html.articulos.messagePassing())
+      case "testing-reactivo" => Ok(views.html.articulos.testingReactivo())
+      case _ => NotFound("Publicación no encontrada")
+    }
+  }
+
+  def portafolio() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.portafolio())
   }
 
   def submitContact() = Action.async { implicit request: Request[AnyContent] =>
@@ -48,5 +70,31 @@ class HomeController @Inject()(
         }
       }
     )
+  }
+
+  // Endpoint opcional para listar contactos (útil para admin)
+  def listContacts(page: Int) = Action.async { implicit request: Request[AnyContent] =>
+    contactRepository.list(page, pageSize = 20).map { contacts =>
+      Ok(play.api.libs.json.Json.toJson(contacts.map { c =>
+        play.api.libs.json.Json.obj(
+          "id" -> c.id,
+          "name" -> c.name,
+          "email" -> c.email,
+          "message" -> c.message,
+          "createdAt" -> c.createdAt.toString,
+          "status" -> c.status
+        )
+      }))
+    }
+  }
+
+  // Endpoint para obtener estadísticas
+  def contactStats() = Action.async { implicit request: Request[AnyContent] =>
+    contactRepository.count().map { total =>
+      Ok(play.api.libs.json.Json.obj(
+        "total" -> total,
+        "timestamp" -> java.time.Instant.now().toString
+      ))
+    }
   }
 }
